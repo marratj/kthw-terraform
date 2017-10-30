89a03606f8953f0298a7e6a0f4b9783ea7ca13ae
Marcel D. Juhnke <marrat@marrat.de>
2017-10-30 19:04:16 +0100

    starting 04-certificate-authority

---
+++ modules/pki/admin_cert.tf
@@ -0,0 +1,45 @@
+resource "tls_private_key" "kube_admin" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "kube_admin" {
+  key_algorithm   = "${tls_private_key.kube_admin.algorithm}"
+  private_key_pem = "${tls_private_key.kube_admin.private_key_pem}"
+
+  subject {
+    common_name         = "admin"
+    organization        = "system:masters"
+    country             = "US"
+    locality            = "Portland"
+    organizational_unit = "Kubernetes The Hard Way"
+    province            = "Oregon"
+  }
+}
+
+resource "tls_locally_signed_cert" "kube_admin" {
+  cert_request_pem   = "${tls_cert_request.kube_admin.cert_request_pem}"
+  ca_key_algorithm   = "${tls_private_key.kube_ca.algorithm}"
+  ca_private_key_pem = "${tls_private_key.kube_ca.private_key_pem}"
+  ca_cert_pem        = "${tls_self_signed_cert.kube_ca.cert_pem}"
+
+  validity_period_hours = 8760
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "cert_signing",
+    "client_auth",
+    "server_auth",
+  ]
+}
+
+resource "local_file" "kube_admin_key" {
+  content  = "${tls_private_key.kube_ca.private_key_pem}"
+  filename = "./tls/admin-key.pem"
+}
+
+resource "local_file" "kube_admin_crt" {
+  content  = "${tls_self_signed_cert.kube_ca.cert_pem}"
+  filename = "./tls/admin.pem"
+}
---
+++ modules/pki/ca.tf
@@ -0,0 +1,41 @@
+# Borrowed from the Tectonic installer: (https://github.com/coreos/tectonic-installer/blob/master/modules/tls/kube/self-signed/ca.tf)
+
+resource "tls_private_key" "kube_ca" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_self_signed_cert" "kube_ca" {
+  key_algorithm   = "${tls_private_key.kube_ca.algorithm}"
+  private_key_pem = "${tls_private_key.kube_ca.private_key_pem}"
+
+  subject {
+    common_name         = "Kubernetes"
+    organization        = "Kubernetes"
+    country             = "US"
+    locality            = "Portland"
+    organizational_unit = "CA"
+    province            = "Oregon"
+  }
+
+  is_ca_certificate     = true
+  validity_period_hours = 8760
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "cert_signing",
+    "client_auth",
+    "server_auth",
+  ]
+}
+
+resource "local_file" "kube_ca_key" {
+  content  = "${tls_private_key.kube_ca.private_key_pem}"
+  filename = "./tls/ca-key.pem"
+}
+
+resource "local_file" "kube_ca_crt" {
+  content  = "${tls_self_signed_cert.kube_ca.cert_pem}"
+  filename = "./tls/ca.pem"
+}
