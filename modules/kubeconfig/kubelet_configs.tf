3c01f5cedce58fb0229d72645ef8622700146037
Marcel D. Juhnke <marrat@marrat.de>
2017-12-09 11:54:37 +0100

    completed kubelet configs

--- modules/kubeconfig/kubelet_configs.tf
+++ modules/kubeconfig/kubelet_configs.tf
@@ -6,4 +6,16 @@ resource "null_resource" "kubelet_configs" {
   provisioner "local-exec" {
     command = "kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=tls/ca.pem --embed-certs=true --server=https://${var.apiserver_public_ip}:6443 --kubeconfig=${element(var.kubelet_node_names, count.index)}.kubeconfig"
   }
+
+  provisioner "local-exec" {
+    command = "kubectl config set-credentials system:node:${element(var.kubelet_node_names, count.index)} --client-certificate=${element(var.kubelet_node_names, count.index)}.pem --client-key=${element(var.kubelet_node_names, count.index)}-key.pem --embed-certs=true --kubeconfig=${element(var.kubelet_node_names, count.index)}.kubeconfig"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl config set-context default --cluster=kubernetes-the-hard-way --user=system:node:${element(var.kubelet_node_names, count.index)} --kubeconfig=${element(var.kubelet_node_names, count.index)}.kubeconfig"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl config use-context default --kubeconfig=${element(var.kubelet_node_names, count.index)}.kubeconfig"
+  }
 }
