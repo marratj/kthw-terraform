e60a5e093e68fa1f6c1a9c396303edede898a299
Marcel Juhnke <marrat@marrat.de>
2019-02-05 17:49:35 +0100

    add verification output

--- modules/etcd/etcd.tf
+++ modules/etcd/etcd.tf
@@ -80,6 +80,7 @@ resource "null_resource" "etcd_server" {
       "sudo systemctl daemon-reload",
       "sudo systemctl enable etcd",
       "sudo systemctl start etcd",
+      "sudo ETCDCTL_API=3 etcdctl member list --endpoints=https://127.0.0.1:2379 --cacert=/etc/etcd/ca.pem --cert=/etc/etcd/kubernetes.pem --key=/etc/etcd/kubernetes-key.pem",
     ]
   }
 }
